8037826903962760cf6f3dfb28ec2b220dad0e1d
Peter Phan <peter_phan05@yahoo.com>
2021-08-23 02:52:05 -0700

    decision section

---
+++ js/index.js
@@ -0,0 +1,19 @@
+
+
+
+
+const pickUserHand = hand => {
+    console.log(hand);
+    //hide the current page
+    let hands = document.querySelector(".hands");
+    hands.style.display = "none";
+
+
+
+
+
+
+
+    //show the next page with the hand you picked
+
+};
